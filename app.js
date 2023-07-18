a38875f919384be443310d51da627db1a3a0b91b
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-07-18 18:24:40 +0600

    Initial Setup Done

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const cors = require("cors");
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const app = express();
+
+// * MiddleWare
+
+app.use(express.json());
+app.use(cors());
+
+// ? Schema
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+      unique: [true, "Name must be unique"],
+      minLenght: [10, "Product Name must be at least 10 characters"],
+      maxLenght: [30, "Product Name cann't be more 30 characters"],
+    },
+    discription: {
+      type: String,
+      required: [true, "Please provide a discription"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide a price"],
+      min: [0, "Price cann't be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      // enum:["kg", "litre","pcs"]
+      enum: {
+        value: ["kg", "litre", "pcs"],
+        massage: "Unit value cann't be {VALUE} ,must be kg/litre/pcs",
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity cann't be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+        message: "Quantity cann't be negative and must be a integer number",
+      },
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        value: ["in stock", "out of Stock", "discontinued"],
+        massage:
+          "Status value cann't be {VALUE},must be in stock/out of Stock/discontinued",
+      },
+    },
+    supplier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier",
+    },
+    catagories: [
+      {
+        name: {
+          type: String,
+          required: [true, "Please provide a name"],
+          unique: [true, "Name must be unique"],
+        },
+        _id: mongoose.Schema.Types.ObjectId,
+      },
+    ],
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // updatedAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+  },
+
+  { timestamps: true, _id: 0 }
+);
+
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+
+const app = require("./app");
+
+//! Server Connection
+
+const port = process.env.PORT || 5000;
+// express
+
+// ! DataBase Connection
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("Connected to database".green.bold))
+  .catch((error) => console.error(error));
+
+app.listen(port, () => {
+  console.log(`App is running on port : ${port}`.yellow.bold);
+});
